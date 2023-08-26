879535ff5559451e32a59176fb8d3592b87da5bd
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-08-26 17:09:39 +0530

    creating react elements

--- Episodes/Ep-1 Inception/App.js
+++ Episodes/Ep-1 Inception/App.js
@@ -3,3 +3,21 @@ const heading=React.createElement("h1",{id:"header_1"},"Hello World with react")
 const root= ReactDOM.createRoot(document.getElementById("root")); //create root
 
 root.render(heading); 
+
+
+const parent=React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"child1"},
+[React.createElement("h1",{}," i am h1 tag"),
+React.createElement("h2",{},"i am h2 tag")
+]),
+
+React.createElement("div",{id:"child2"},
+[React.createElement("h1",{}," i am h1 tag"),
+React.createElement("h2",{},"i am h2 tag")
+])
+]
+);
+
+console.log(parent);
+root.render(parent);
