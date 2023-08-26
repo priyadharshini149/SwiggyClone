adc3dbf1b7e7011c95c9a9ba79020313d9cd6a73
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-08-26 16:30:38 +0530

    adding react into html

---
+++ Episodes/Ep-1 Inception/App.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{id:"header_1"},"Hello World with react"); //created h1 tag
+
+const root= ReactDOM.createRoot(document.getElementById("root")); //create root
+
+root.render(heading); 
