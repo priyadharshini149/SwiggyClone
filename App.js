30835910126898ace03874b44fbc51e150e43df5
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-09-02 21:22:25 +0530

    ep-2 igniting app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 const heading=React.createElement("h1",{id:"header_1"},"Hello World with react"); //created h1 tag
 
 const root= ReactDOM.createRoot(document.getElementById("root")); //create root
