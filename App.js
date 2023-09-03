2eaf2ca0037ecc5cd6c27086ebfc561d8d022c33
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-09-03 23:05:36 +0530

    ep 3 laying foundation

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-const heading=React.createElement("h1",{id:"header_1"},"Hello World with react"); //created h1 tag
-
+//const heading=React.createElement("h1",{id:"header_1"},"Hello World with react"); //created h1 tag
+const jsxheading=<h1>Hello World with react</h1>
 const root= ReactDOM.createRoot(document.getElementById("root")); //create root
 
-root.render(heading); 
+root.render(jsxheading); 
 
 
-const parent=React.createElement("div",{id:"parent"},
-[
-    React.createElement("div",{id:"child1"},
-[React.createElement("h1",{}," i am h1 tag"),
-React.createElement("h2",{},"i am h2 tag")
-]),
+// const parent=React.createElement("div",{id:"parent"},
+// [
+//     React.createElement("div",{id:"child1"},
+// [React.createElement("h1",{}," i am h1 tag"),
+// React.createElement("h2",{},"i am h2 tag")
+// ]),
 
-React.createElement("div",{id:"child2"},
-[React.createElement("h1",{}," i am h1 tag"),
-React.createElement("h2",{},"i am h2 tag")
-])
-]
-);
+// React.createElement("div",{id:"child2"},
+// [React.createElement("h1",{}," i am h1 tag"),
+// React.createElement("h2",{},"i am h2 tag")
+// ])
+// ]
+// );
 
-console.log(parent);
-root.render(parent);
+// console.log(parent);
+// root.render(parent);
