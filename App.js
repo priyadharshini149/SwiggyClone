15764ec5e5b8b48fea6a9a61b1df0b180489389a
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-09-04 21:16:32 +0530

    ep 3 react component

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 //const heading=React.createElement("h1",{id:"header_1"},"Hello World with react"); //created h1 tag
-const jsxheading=<h1>Hello World with react</h1>
+// const jsxheading=<h1>Hello World with react</h1>
+
+//react element
+const heading=<h1>Hello world with react element !!!</h1>
+
+const Title = () => (
+    <h1>Hello world</h1>
+);
+//react functional component
+const Heading= () => (
+    <div>
+        <Title/> 
+        <h1>Hello world with react component !!!</h1>
+    </div>
+    
+);// component composition
+
+
 const root= ReactDOM.createRoot(document.getElementById("root")); //create root
 
-root.render(jsxheading); 
+root.render(<Heading/>);  //rendering react component
 
 
 // const parent=React.createElement("div",{id:"parent"},
