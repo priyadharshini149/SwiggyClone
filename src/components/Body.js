2a0af45876d3455cb8e1d4a03145940ba2ba25fd
Priyadharshini S <priyadharshini.sathasivam@multicorewareinc.com>
2023-09-06 19:41:37 +0530

    filter is added to the function

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import RestoCard from "./RestoCard";
 import resList from "../utils/mockData";
+
   
 const Body = () => {
+    const [listOfResto,setListofResto]=useState(resList);
     return (
         <div className="body">
             <div className="search">
@@ -9,10 +12,37 @@ const Body = () => {
                   <input type="text" placeholder="search"></input>
                 </form>
             </div>
+            <div className="filter">
+                <div>
+                    <h4>Filter Based On</h4>
+                </div>
+                <div>
+                    <button
+                    onClick={()=>{
+                        setListofResto(resList);
+                    }}
+                     >
+                        All
+                    </button>
+                </div>
+                <div>
+                    <button 
+                    onClick={()=>{
+                        const filterlist=listOfResto.filter((res) => res.info.avgRating>4);
+                        setListofResto(filterlist);
+                    }}
+                    >
+                        Top rated restaurant </button>
+                </div>
+                <div className="Rate">
+                    <button>Rate </button>
+                </div>
+
+            </div>
             <div className="resto-container">
               {/* <RestoCard name="Meghana Foods" cusine="Biryani, north indian,asian" rating="4.5 stars" duration="35 minutes"/>
                <RestoCard name="KFC" cusine="Biryani, north indian,asian" rating="4.3 stars" duration="30 minutes"/> */}
-               {resList.map(restaurant=>(
+               {listOfResto.map(restaurant=>(
                <RestoCard key={restaurant.info.id} resData={restaurant}/>
                ))}
                
