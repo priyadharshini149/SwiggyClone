fc5d3a2fcb268158fb99e8e1eb9d505b476fb73c
Priyadharshini S <priyadharshinisathasivam01@gmail.com>
2023-09-17 09:14:25 +0530

    resto card style is modified

--- src/components/RestoCard.js
+++ src/components/RestoCard.js
@@ -8,7 +8,12 @@ const RestoCard = (props) => {
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
     resData?.info;
   const { deliveryTime } = resData.info.sla;
-  const{header,subHeader}=resData.info.aggregatedDiscountInfoV3;
+  {
+    resData.info.aggregatedDiscountInfoV3 && (
+      {header,subHeader}=resData.info.aggregatedDiscountInfoV3
+    )
+  }
+
 
   const [rateColor, setrateColor] = useState("green");
   useEffect(() => {
@@ -24,17 +29,21 @@ const RestoCard = (props) => {
       }, [avgRating]);
   return (
     <div className="resto-card">
-      <div className="layer"><h4>{header} {subHeader}</h4></div>
+      <div className="layer">
+        { header && subHeader&&<h4>{header} {subHeader}</h4>}
+        </div>
       <img
         src={CDN_URL + cloudinaryImageId}
         className="food-img"
         alt="image"
       ></img>
       <h3>{name}</h3>
-      <div className="card-review">
+      <div className="card-review" style={{display:"inline-flex"}}>
       
       <span style={{ backgroundColor:  rateColor  }}>
         <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>  {avgRating}</span>
+
+      <h5 style={{padding:"0.25rem",marginLeft:"2px"}}>{deliveryTime} mins</h5>
       </div>
       <h4>{cuisines.join(", ")}</h4>
     </div>
