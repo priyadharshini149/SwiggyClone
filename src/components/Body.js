ea065b211b2e4008e002b67b09b14f24358df2e0
Priyadharshini S <priyadharshinisathasivam01@gmail.com>
2023-09-19 19:57:57 +0530

    collection data are imported

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,9 +9,28 @@ const Body = () => {
     const [filterResto,setFilterResto]=useState([]);
     const [searchText,setSearchText]=useState("");
     const[offerBanner,setofferBanner]=useState(null);
+    const [collection,setCollection]=useState([]);
     useEffect(()=>{ 
         fetchData();
+        
     },[])
+    function getCollectionId(url)
+        {
+            try{
+                const match=url.match(/collection_id=([^&]*)/)
+                if(match&& match[1])
+                {
+                    return match[1]
+                }
+                else{
+                    return null
+                }
+            }
+            catch(error){
+                console.error("Error parsing URL:", error);
+        return null;
+            }
+        }
     const fetchData= async()=>{
         const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.0261194&lng=77.0191128&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json= await data.json();
@@ -19,6 +38,7 @@ const Body = () => {
         setListofResto(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         setFilterResto(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         setofferBanner(json?.data.cards[0]?.card.card.gridElements?.infoWithStyle?.info)
+        setCollection(json?.data?.cards[1]?.card?.card.imageGridCards.info);
     };
      
     console.log(listOfResto)
@@ -29,23 +49,13 @@ const Body = () => {
     // }
     return listOfResto.length===0? <Shimmer/> : (
         <div className="body">
-            <div className="search">
-                
-                  <input type="text" placeholder="search" value={searchText} onChange={(e)=>{
-                    setSearchText(e.target.value);
-                   
-                  }} ></input>
-                  <button className="search-btn" onClick={()=>{
-                     console.log(searchText);
-                    const searchFilter=listOfResto.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()) || res.info.cuisines.join(" ").toLowerCase().includes(searchText.toLowerCase()));
-                   
-                    setFilterResto(searchFilter);
-                   
-                  }}>search</button>
-                
-            </div>
+
+           
+
+
+            {/* offer banner */}
             <div className="offer-container">
-            <h3>Best offers for you</h3>
+            <h1>Best offers for you</h1>
             <div className="Offer-banner">
                 
               {
@@ -57,7 +67,42 @@ const Body = () => {
               }
             </div>
             </div>
-           
+            <div className="collection-container">
+              <h1>What's on your mind?</h1>
+              <div className="collection">
+                {
+                    collection.map((col,index)=>(
+                        <div key={index} >
+                        <Link to={"collection/"+(getCollectionId(col.entityId)||col.entityId)+"/"+col.action.text}>
+                            <img className="coll" src={CDN_URL+col.imageId}></img>
+                        </Link>
+                        </div>
+                    ))
+                }
+              </div>
+            </div>
+           <hr></hr>
+           <div className="restaurants">
+            <div className="rest-head">
+           <h1> Restaurants with online food delivery</h1>
+             {/* search */}
+             <div className="search">
+                
+                <input type="text" placeholder="search" value={searchText} onChange={(e)=>{
+                  setSearchText(e.target.value);
+                 
+                }} ></input>
+                <button className="search-btn" onClick={()=>{
+                   console.log(searchText);
+                  const searchFilter=listOfResto.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()) || res.info.cuisines.join(" ").toLowerCase().includes(searchText.toLowerCase()));
+                 
+                  setFilterResto(searchFilter);
+                 
+                }}>search</button>
+              
+            </div>
+
+            {/* filter */}
             <div className="filter">
                 <div>
                     <h4>Filter Based On</h4>
@@ -82,17 +127,28 @@ const Body = () => {
                 </div>
 
             </div>
+
+           
+            </div>
+            {/* Resto card */}
+           
             <div className="resto-container">
+           
               {/* <RestoCard name="Meghana Foods" cusine="Biryani, north indian,asian" rating="4.5 stars" duration="35 minutes"/>
                <RestoCard name="KFC" cusine="Biryani, north indian,asian" rating="4.3 stars" duration="30 minutes"/> */}
                {filterResto.map(restaurant=>(
-                <Link key={restaurant.info.id} to={"restaurants/"+restaurant.info.id}>
-                    
+                <Link key={restaurant.info.id} to={"restaurants/"+restaurant.info.id}> 
                     <RestoCard  resData={restaurant}/>
                 </Link>
                ))}
                
             </div>
+
+           </div>
+                <hr></hr>
+           
+
+            
 
         </div>
     );
